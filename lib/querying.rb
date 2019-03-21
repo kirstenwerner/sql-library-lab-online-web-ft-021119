986cf5a039a2ba8e1c341a2dc35fff9b3d38ff19
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-21 23:58:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_series_title_with_most_human_characters
   JOIN books ON series.id = books.series_id
   JOIN character_books ON books.id = character_books.book_id
   JOIN characters ON character_books.character_id = characters.id
-  GROUP BY characters.species"
+  GROUP BY COUNT(characters.species) AS value_occurance"
 end
 
 def select_character_names_and_number_of_books_they_are_in
