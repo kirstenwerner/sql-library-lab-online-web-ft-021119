43a280db36f0798add63624a4def3e02e93a699f
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-21 23:55:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,9 @@ def select_series_title_with_most_human_characters
   JOIN books ON series.id = books.series_id
   JOIN character_books ON books.id = character_books.book_id
   JOIN characters ON character_books.character_id = characters.id
-  ORDER by COUNT(characters.species) AS value_occurance FROM characters"
+  ORDER BY COUNT(characters.species) AS value_occurance FROM characters 
+  GROUP BY characters.species
+  ORDER BY value_occurance DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
