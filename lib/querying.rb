005b0ff42c7853e13a38a7ce023cc5aaf813d12d
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-22 00:17:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,5 +32,4 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   JOIN character_books ON characters.id = character_books.character_id
   GROUP BY character_books.book_id
-  ORDER BY value_occurance, characters.name"
 end
